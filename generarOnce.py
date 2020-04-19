d68cebee7c7fc73c05000420a8a1f039e608ce82
mmorac <marioemorac@gmail.com>
2020-04-19 04:15:16 -0600

    Información de las distintas bases

--- generarOnce.py
+++ generarOnce.py
@@ -1,12 +1,38 @@
 import obtenerInformacion
-
+import pandas as pd
 
 
 #Lista de clubes
-clubes = {1:"Saprissa", 2:"Alajuelense", 3:"Herediano", 4:"Cartaginés", 5:"Pérez Zeledón", 6:"Santos", 7:"San Carlos", 8:"Belén", 9:"Puntarenas",
-10:"Limón", 11:"Carmelita", 12:"Uruguay", 13:"La U", 14:"As Puma Generaleña", 15:"Liberia", 16: "Guadalupe FC", 17:"Municipal Grecia", 18:"Jicaral Sercoba"}
+clubes = {1:"SAP", 2:"LDA", 3:"CSH", 4:"CSC", 5:"MPZ", 6:"SAN", 7:"ADSC", 8:"BEL", 9:"PUN",
+10:"LFC", 11:"ADC", 12:"CSU", 13:"LAU", 14:"ASP", 15:"LIB", 16: "GFC", 17:"GRE", 18:"JIC"}
 
 key = obtenerInformacion.obtenerApiKey()
+
 descargar = False
 
-obtenerInformacion.obtenerInformacion(descargar, key)
+#Se obtiene la base de datos consolidada
+info = obtenerInformacion.obtenerInformacion(descargar, key)
+
+#Se obtiene la lista de jugadores con la respectiva posición
+jugadores = obtenerInformacion.obtenerJugadores()
+
+
+jornada = int(input("Elija una jornada de inicio:"))
+jornada_fin = int(input("Elija una jornada de cierre:"))
+
+
+reg_jornada = info.loc[(info['id_jornada'] >= float(jornada)) & (info['id_jornada'] <= float(jornada_fin))]
+
+porteros = {}
+defensas = {}
+volantes = {}
+delanteros = {}
+
+
+once = reg_jornada.sort_values(["IRJ_General"], ascending = False)
+
+for index, fila in once.iterrows():
+    info_basica = jugadores.loc[jugadores["IDJugador"] == fila["id_jugador"]]
+
+
+once = once.head(11)
--- obtenerInformacion.py
+++ obtenerInformacion.py
@@ -8,37 +8,36 @@ import pandas as pd
 digitadores = ["Alejandro", "Cristian", "Gabriel", "Greivin", "Gustavo", "Irving", "Jonathan", "Mario"]
 
 def obtenerApiKey():
-    f=open("api_key.txt", "r")
+    f=open(os.environ['USERPROFILE'] + "/Documents/SIFUTBases/" + "api_key.txt", "r")
     key = f.read()
     return key
 
 
 def obtenerInformacion(descarga_requerida, api_key):
-    dbx = dropbox.Dropbox(api_key)
+    api_key = api_key.strip()
+
+    try:
+        dbx = dropbox.Dropbox(api_key)
+    except ValueError as ve:
+        print(ve.message)
+
+    info = pd.DataFrame({})
 
     if(descarga_requerida):
         #Se recorre la lista de digitadores para descargar sus archivos respectivos
         for digitador in digitadores:
-            with open(digitador + ".xlsx", "wb") as f:
+            with open(os.environ['USERPROFILE'] + "/Documents/SIFUTBases/" + digitador + ".xlsx", "wb") as f:
                 #Se usará el método files_download de la librería de DROPBOX para la descarga
                 metadata, res = dbx.files_download(path="/2020 Torneo de Clausura/Control de jornadas de jugadores y clubes TC 2020 " + digitador + ".xlsx")
                 f.write(res.content)
                 print("¡Archivo de", digitador, "exitosamente descargado!")
 
 
-    df = pd.read_excel(r"Mario.xlsx", "Jugadores")
-    id = 1
-    while(id != 0):
-        try:
-            id = int(input("Ingrese un ID de jugador: "))
-        except ValueError:
-            print("Debe ingresar un valor numérico entero")
-            id = int(input("Ingrese un ID de jugador: "))
+    for digitador in digitadores:
+        df = pd.read_excel(os.environ['USERPROFILE'] + "/Documents/SIFUTBases/" + digitador + ".xlsx", "Jugadores")
+        info = pd.concat([info, df])
+    return info
 
-        if(df.id_condicion[id] == "1"):
-            condicion = "local"
-        else:
-            condicion = "visitante"
-
-        jugadores = [df.NombreJugador[id], df.IRJ_General[id], df.id_club[id], condicion, df.id_jornada[id]]
-        print(jugadores[0], "de", jugadores[2], "logró un IRJ de:", jugadores[1], "en su partido como", jugadores[3], "de la jornada", jugadores[4])
+def obtenerJugadores():
+    df = pd.read_excel(os.environ['USERPROFILE'] + "/Documents/SIFUTBases/Listado de Jugadores con Código TC 2020.xlsx", "Listado jugadores")
+    return df
